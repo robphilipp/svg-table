43a45d6cd34cad1929687f801c1c66c632b8c29c
Robert Philipp <rob.philipp@gmail.com>
2025-09-07 08:52:40 -0400

    refactoring to library

---
+++ index.ts
@@ -0,0 +1,15 @@
+export {
+    DataFrame,
+    CellValue
+} from './DataFrame';
+export type {
+    TagCoordinate,
+    Tag,
+    RowTag, ColumnTag, CellTag,
+    TagValue
+} from './tags'
+export {
+    RowCoordinate, ColumnCoordinate, CellCoordinate,
+    Tags,
+    newTag, newRowTag, newColumnTag, newCellTag
+} from './tags'
